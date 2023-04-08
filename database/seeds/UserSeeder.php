f2ba1327e4ab363ebffef13e1fe76b4cde19d7b3
Simone823 <simone.daglio823@outlook.it>
2023-04-08 09:00:47 +0200

    user seeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,61 @@
+<?php
+
+use App\Role;
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // roles
+        $roleAdmin = Role::where('name', '=', 'Admin')->first();
+        $roleManager = Role::where('name', '=', 'Manager')->first();
+        $roleUser = Role::where('name', '=', 'User')->first();
+
+        // users array
+        $users = array(
+            [
+                'name' => 'admin',
+                "username" => 'admin',
+                'email' => 'admin@pwdmanager.test',
+                'password' => 'password',
+                "role_id" => $roleAdmin->id
+            ],
+            [
+                'name' => 'manager',
+                "username" => 'manager',
+                'email' => 'manager@pwdmanager.test',
+                'password' => 'password',
+                "role_id" => $roleManager->id
+            ],
+            [
+                'name' => 'simone',
+                "username" => 'simone',
+                'email' => 'simone@pwdmanager.test',
+                'password' => 'password',
+                "role_id" => $roleUser->id
+            ]
+        );
+
+        foreach ($users as $key => $user) {
+            // una istanza di User
+            $newUser = new User();
+
+            // setto i valori
+            $newUser->name = $user['name'];
+            $newUser->username = $user['username'];
+            $newUser->email = $user['email'];
+            $newUser->password = Hash::make($user['password']);
+            $newUser->role_id = $user['role_id'];
+
+            // save
+            $newUser->save();
+        }
+    }
+}
