2b3f29494cbb745445fa25d903b159901581f1a9
Simone823 <simone.daglio823@outlook.it>
2023-04-08 12:12:48 +0200

    loader page document

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
 require('./bootstrap');
+
+// div loader
+const divLoader = document.getElementById('loader');
+
+// div page loaded
+const divPageLoaded = document.getElementById('page-loaded');
+
+
+// document load event
+document.addEventListener('readystatechange', function(e) {
+    if(document.readyState == 'complete') {
+        setTimeout(() => {
+            divLoader.classList.remove('d-block');
+            divLoader.classList.add('d-none');
+
+            divPageLoaded.classList.remove('d-none');
+        }, 250);
+    }
+});
