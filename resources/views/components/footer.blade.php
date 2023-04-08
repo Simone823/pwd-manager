bf8de8babe93585a451fbc935de1d2ccdee34d64
Simone823 <simone.daglio823@outlook.it>
2023-04-08 10:55:57 +0200

    footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,18 @@
+<footer class="position-fixed bottom-0 w-100 py-2 bg-dark text-violet">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-12 d-flex align-items-center justify-content-between">
+                {{-- copryright --}}
+                <h6 class="mb-0 d-flex flex-wrap gap-2">
+                    &copy;Copyright {{date('Y')}}
+                    <a class="link-violet text-decoration-none fs-6" target="_blank" href="https://simonedaglio.it" alt="Simone Daglio">@Simone Daglio</a>
+                </h6>
+
+                {{-- version app --}}
+                <h6 class="mb-0">v {{config('app.version')}}</h6>
+            </div>
+
+        </div>
+    </div>
+</footer>
