0d765c8872be2ce45a2decc61f7415f732e38c0b
Simone823 <simone.daglio823@outlook.it>
2023-04-08 10:28:19 +0200

    loader page component

---
+++ resources/views/components/loader.blade.php
@@ -0,0 +1,4 @@
+<div id="loader" class="vw-100 vh-100 d-flex justify-content-center align-items-center d-block">
+    <p class="m-0 p-0 text-dark">.</p>
+    <span class="loader-spinner"></span>
+</div>
