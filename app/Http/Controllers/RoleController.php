c7442b42743ff3240e3612595300c2a92e9cef92
Simone823 <simone.daglio823@outlook.it>
2023-04-08 19:06:12 +0200

    role controller,index destroy ,route resource

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Role;
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('hasRole:admin');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // get all roles orber by name asc
+        $roles = Role::orderBy('name', 'asc')->paginate(10);
+
+        return view('roles.index', compact('roles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // recupero il ruolo by id
+        $role = Role::find($id);
+
+        // cancello il ruolo
+        $role->delete();
+
+        return redirect()->route('roles.index')->with('success', "Il Ruolo con nome: {$role->name} è stato eliminato");
+    }
+}
---
+++ resources/views/roles/index.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.app')
+
+@section('title', '| Lista Ruoli')
+
+@section('content')
+    <section id="roles-index" class="pt-4">
+        <div class="container">
+            <div class="card bg-dark py-4 px-3 shadow">
+
+                {{-- row title --}}
+                <div class="row mb-5">
+                    <h2 class="mb-0 text-violet fs-4 fw-bold">Lista Ruoli</h2>
+                </div>
+
+                {{-- row table roles --}}
+                <div class="row">
+                    <div class="col-12 table-responsive">
+                        <table class="table table-dark table-striped shadow">
+                            <thead>
+                                <tr>
+                                    <th scope="col"></th>
+                                    <th scope="col">Nome</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($roles as $role)    
+                                    <tr>
+                                        <th>
+                                            {{-- btn delete --}}
+                                            @if ($role->name != 'Admin')
+                                                <div class="btn-delete">
+                                                    <form action="{{route('roles.destroy', $role->id)}}" method="POST">
+                                                        @csrf
+                                                        @method('DELETE')
+
+                                                        <button type="submit" class="btn btn-violet shadow">
+                                                            <i class="fa-solid fa-trash"></i>
+                                                        </button>
+                                                    </form>
+                                                </div>
+                                            @endif
+                                        </th>
+                                        <th class="fw-normal">{{$role->name}}</th>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                {{-- row link page --}}
+                <div class="row">
+                    <div class="links-paginate col-12 d-flex justify-content-center mt-4">
+                        {!! $roles->appends(\Request::except('page'))->render() !!}
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+@endsection
