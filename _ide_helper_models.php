4cbc50fbf5fe046df31673f152c5ed5e75531f95
Simone823 <simone.daglio823@outlook.it>
2023-04-12 18:00:13 +0200

    update ide helper

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -107,12 +107,12 @@ namespace App{
  * @property \Illuminate\Support\Carbon|null $email_verified_at
  * @property string $password
  * @property string|null $remember_token
- * @property int $role_id
+ * @property int|null $role_id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
  * @property-read int|null $notifications_count
- * @property-read \App\Role $role
+ * @property-read \App\Role|null $role
  * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|User query()
