51ff4baddbc61af0a06be93bb6cc850917ea1972
Simone823 <simone.daglio823@outlook.it>
2023-04-12 17:59:41 +0200

    rotte users, categories, clients

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,22 @@ Route::middleware('auth')->group(function () {
     // Rotta default /
     Route::get('/', 'HomeController@index')->name('home');
 
+    // Rotte categorie
+    Route::get('/categories/index', 'CategoryController@index')->middleware('hasPermission:categories-view')->name('categories.index');
+    Route::get('/categories/create', 'CategoryController@create')->middleware('hasPermission:categories-create')->name('categories.create');
+    Route::post('/categories/store', 'CategoryController@store')->middleware('hasPermission:categories-create')->name('categories.store');
+    Route::get('/categories/edit/{id}', 'CategoryController@edit')->middleware('hasPermission:categories-edit')->name('categories.edit');
+    Route::put('/categories/update/{id}', 'CategoryController@update')->middleware('hasPermission:categories-edit')->name('categories.update');
+    Route::delete('/categories/delete/{id}', 'CategoryController@destroy')->middleware('hasPermission:categories-delete')->name('categories.destroy');
+
+    // Rotte clienti
+    Route::get('/clients/index', 'ClientController@index')->middleware('hasPermission:clients-view')->name('clients.index');
+    Route::get('/clients/create', 'ClientController@create')->middleware('hasPermission:clients-create')->name('clients.create');
+    Route::post('/clients/store', 'ClientController@store')->middleware('hasPermission:clients-create')->name('clients.store');
+    Route::get('/clients/edit/{id}', 'ClientController@edit')->middleware('hasPermission:clients-edit')->name('clients.edit');
+    Route::put('/clients/update/{id}', 'ClientController@update')->middleware('hasPermission:clients-edit')->name('clients.update');
+    Route::delete('/clients/delete/{id}', 'ClientController@destroy')->middleware('hasPermission:clients-delete')->name('clients.destroy');
+
     // Rotte middleware role admin
     Route::middleware('hasRole:admin')->group(function () {
         // Rotte permissions
@@ -29,5 +45,8 @@ Route::middleware('auth')->group(function () {
 
         // Rotte roles
         Route::resource('/roles', 'RoleController');
+
+        // Rotte users
+        Route::resource('/users', 'UserController');
     });
 });
