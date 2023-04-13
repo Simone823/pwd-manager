73953a57f5d54ebf5fc372a2922bed337eb20bfd
Simone823 <simone.daglio823@outlook.it>
2023-04-13 20:23:00 +0200

    bg dark card home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
+                <div class="card bg-dark text-light-gray">
                     <div class="card-header">{{ __('Dashboard') }}</div>
     
                     <div class="card-body">
