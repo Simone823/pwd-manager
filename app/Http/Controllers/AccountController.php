67f5d8ce2967f9009b60ae6e251de5f4fbbf270c
Simone823 <simone.daglio823@outlook.it>
2023-04-13 20:23:42 +0200

    account controller

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Account;
+use App\Category;
+use App\Client;
+use Crypt;
+use Illuminate\Http\Request;
+
+class AccountController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // recupero tuti gli account dal db
+        $accounts = Account::orderBy('created_at', 'desc')->paginate(10);
+
+        return view('accounts.index', compact('accounts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // recupero tutti i clienti dal db
+        $clients = Client::orderBy('name', 'asc')->get();
+
+        // recupero tutte le categorie dal db
+        $categories = Category::orderBy('name', 'asc')->get();
+
+        return view('accounts.create', compact('clients', 'categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // validazione request
+        $request->validate([
+            'name' => 'required|string|min:4|max:230',
+            'client_id' => 'required|exists:clients,id',
+            'category_id' => 'required|exists:categories,id',
+            'url' => 'nullable|url',
+            'username' => 'required|string|min:4|max:255',
+            'password' => 'required|string|min:8|confirmed',
+            'description' => 'nullable|string'
+        ]);
+
+        // data reuqest all
+        $data = $request->all();
+
+        // creo nuova istanza Account
+        $newAccount = new Account();
+
+        // setto i valori
+        $newAccount->name = ucwords($data['name']);
+        $newAccount->client_id = $data['client_id'];
+        $newAccount->category_id = $data['category_id'];
+        $newAccount->url = $data['url'];
+        $newAccount->username = $data['username'];
+        $newAccount->password = Crypt::encryptString($data['password']);
+        $newAccount->description = $data['description'];
+
+        // save
+        $newAccount->save();
+
+        return redirect()->route('accounts.index')->with('success', "L'Account con nome: {$newAccount->name} è stato creato.");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
