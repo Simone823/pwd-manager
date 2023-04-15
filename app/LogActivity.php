506680c70d1e0506f1bce23e6e74e3b0ddf7ae2f
Simone823 <simone.daglio823@outlook.it>
2023-04-15 09:09:14 +0200

    log activity model addlog

---
+++ app/LogActivity.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App;
+
+use App\LogActivity as AppLogActivity;
+use Auth;
+use Illuminate\Database\Eloquent\Model;
+use Request;
+
+class LogActivity extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'action',
+        'url', 
+        'method',
+        'ip',
+        'agent',
+        'user_id'
+    ];
+
+
+    /**
+     * AGGIUNGI UN LOG SUL DB TABELLA LOG ACTIVITY
+     *
+     * @param string $actionName
+     * @return void
+     */
+    public static function addLog(string $actionName): void
+    {
+        // new log
+        $newLog = new AppLogActivity();
+
+        // setto i valori
+        $newLog->action = ucwords($actionName);
+        $newLog->url = Request::fullUrl();
+        $newLog->method = Request::method();
+        $newLog->ip = Request::ip();
+        $newLog->agent = Request::header('user-agent');
+        $newLog->user_id = Auth::check() ? Auth::user()->id : null;
+
+        // save
+        $newLog->save();
+    }
+}
