2a8f6ced8a4010d508d2e4bd426e8a0cd18417d6
Simone823 <simone.daglio823@outlook.it>
2023-12-18 21:24:32 +0100

    fix

--- resources/views/components/loader.blade.php
+++
@@ -1,9 +0,0 @@
-<div id="loader">
-    <div class="container h-100">
-        <div class="row h-100">
-            <div class="col-12 d-flex justify-content-center align-items-center">
-                <span class="loader-spinner"></span>
-            </div>
-        </div>
-    </div>
-</div>
